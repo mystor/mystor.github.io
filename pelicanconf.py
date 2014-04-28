ec3b0bab299d9ca3975448ea6bb7cff6f623ea56
Michael Layzell <michael@thelayzells.com>
2014-04-28 11:55:43 -0600

    initial pelican site setup

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Michael Layzell'
+SITENAME = u'Mystor\'s Box'
+SITEURL = ''
+TAGLINE = 'Code & stuff'
+COVER_IMG_URL = '/images/michael.png'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('GitHub Release Watch', 'http://gh-release-watch.com'),)
+
+# Social widget
+SOCIAL = (('github', 'https://github.com/mystor'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME='./pure-single'
