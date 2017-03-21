3ef2833ab4915bb03724d5ea0551d97eb6b0eae3
Michael Layzell <michael@thelayzells.com>
2017-03-20 20:30:04 -0400

    Add wouldnt-it-be-neat-p1 blogpost

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,12 +4,12 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Michael Layzell'
 SITENAME = u'Mystor\'s Box'
-SITEURL = ''
-TAGLINE = 'Code & stuff'
+SITEURL = 'https://mystor.github.io'
+TAGLINE = 'Software Engineer at Mozilla.<br>I work on random stuff in C++ and Rust.'
 # COVER_IMG_URL = '/images/squidhat.png'
 PROFILE_IMG_URL = '/images/squidhat.png'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'America/Toronto'
 
 DEFAULT_LANG = u'en'
 
@@ -19,16 +19,16 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('GitHub Release Watch', 'http://gh-release-watch.com'),)
+LINKS =  ()
 
 # Social widget
 SOCIAL = (('inbox', 'mailto:michael@thelayzells.com'),
           ('github', 'https://github.com/mystor'),
-          ('twitter', 'https://twitter.com/mistron'),)
+          ('twitter', 'https://twitter.com/layzellm'),)
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 THEME='./pure-single'
